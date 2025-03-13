2e7604018cfcb6cfbc1810eeada8eac99b18defd
Ashish Pandey <writeashish22@gmail.com>
2025-03-13 06:14:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,43 @@
-//complete this code
-class Animal {}
+// Base Animal class
+class Animal {
+  constructor(species) {
+    this._species = species;
+  }
 
-class Dog extends Animal {}
+  // Getter for species
+  get species() {
+    return this._species;
+  }
 
-class Cat extends Animal {}
+  // Method to log the sound message
+  makeSound() {
+    console.log(`The ${this._species} makes a sound`);
+  }
+}
+
+// Dog class extending Animal
+class Dog extends Animal {
+  constructor(species) {
+    super(species);
+  }
+
+  // Method specific to Dog
+  bark() {
+    console.log("woof");
+  }
+}
+
+// Cat class extending Animal
+class Cat extends Animal {
+  constructor(species) {
+    super(species);
+  }
+
+  // Method specific to Cat
+  purr() {
+    console.log("purr");
+  }
+}
 
 // Do not change the code below this line
 window.Animal = Animal;
